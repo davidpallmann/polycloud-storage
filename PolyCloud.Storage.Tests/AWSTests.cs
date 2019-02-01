5f10b54f9e287bfdae223304f278fb61b23352f6
David Pallmann <dpallmann@hotmail.com>
2019-02-01 06:31:46 -0800

    Initial check-in - .NET Framework API

---
+++ PolyCloud.Storage.Tests/AWSTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PolyCloud.Storage.Tests
+{
+    [TestClass]
+    public class AWSTests
+    {
+        [TestCategory("AWS")]
+        [TestMethod]
+        public void CreateFolderAndFile()
+        {
+            Console.WriteLine("Test: AWS | CreateFolderAndFile");
+
+            // Set your storage account credentials here before running tests
+
+            String accessKey = "BKIAIBVVWJCZIPZQM2ZA";
+            String secretKey = "xQlSUFBSLr5x3FvyccBJy5rcD3e93Ftwzic18gQH";
+            String endpoint = "us-east-1";
+
+            using (Storage storage = Storage.AWS(accessKey, secretKey, endpoint))
+            {
+                storage.HandleErrors = false;
+                storage.Open();
+
+                // Create folder
+
+                String folderName = "test-" + Guid.NewGuid().ToString();
+                Console.WriteLine("Creating folder " + folderName);
+                storage.NewFolder(folderName);
+
+                // Create file in folder
+
+                String fileName = "test.txt";
+                String fileName2 = "test2.txt";
+
+                Console.WriteLine("Creating file " + fileName);
+
+                String content = "This is a test.";
+
+                if (File.Exists(fileName)) File.Delete(fileName);
+                File.WriteAllText(fileName, content);
+
+                // Upload file
+
+                Console.WriteLine("Uploading file " + fileName);
+
+                storage.UploadFile(folderName, fileName);
+
+                // Retrieving file
+
+                Console.WriteLine("Downloading file " + fileName + " as " + fileName2);
+
+                if (File.Exists(fileName2)) File.Delete(fileName2);
+                storage.DownloadFile(folderName, fileName, fileName2);
+
+                // Retrieve file
+
+                Console.WriteLine("Downloading file " + fileName + " as " + fileName2);
+
+                if (File.Exists(fileName2)) File.Delete(fileName2);
+                storage.DownloadFile(folderName, fileName, fileName2);
+
+                // Delete file
+
+                storage.DeleteFile(folderName, fileName);
+
+                // Delete folder
+
+                Console.WriteLine("Deleting test folder");
+                storage.DeleteFolder(folderName);
+
+                Console.WriteLine("Verifying content");
+
+                String content2 = File.ReadAllText(fileName2);
+                Assert.AreEqual(content, content2, "Downloaded file content did not match uploaded file content");
+
+                storage.Close();
+            }
+        }
+    }
+
+
+}
---
+++ PolyCloud.Storage.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolyCloud.Storage.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolyCloud.Storage.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2471675f-ef00-4f4a-a08e-80bc68ba6a71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
